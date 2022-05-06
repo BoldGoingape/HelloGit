7832e9683b3939c5eebece787c91f0d0078897c4
wwj-glb <goodMorning-glb@wenju.com>
2022-05-06 23:26:10 +0800

    更新1

---
+++ HelloGIt/src/main/java/wenjuju.top/wenjuju/top/HelloGit.java
@@ -0,0 +1,10 @@
+package wenjuju.top;
+
+public class HelloGit {
+    public static void main(String[] args){
+        System.out.println();
+        for (int i = 0; i < 10; i++) {
+            System.out.println("这个数是"+i);
+        }
+    }
+}
---
+++ HelloGIt/src/main/java/wenjuju.top/wenjuju/top/helloServer.java
@@ -0,0 +1,7 @@
+package wenjuju.top;
+
+public class helloServer {
+    public static void main(String[] args){
+        System.out.println("我是Git");
+    }
+}
